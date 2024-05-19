99ef7df673c7d08d24cac8ba5a7cd072a17f933c
Branden Phan <branden.phan@gmail.com>
2024-05-19 03:51:04 -0400

    Now we are finally finished

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,8 @@ import Logo from "./RecapifyLogo.png";
 
 const App: React.FC = () => {
   return (
-    <div style={{padding: "10px 20px", width: "90%" }}>
-      <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+    <div style={{width: "100%" }}>
+      <div style={{ display: "flex", flexDirection: "row", alignItems: "center", marginTop: "-3px" }}>
         <img src={Logo} alt="RecapifyLogo" height="25px" width="25px" style={{ marginBottom: "3px" }} />
         <p style={{ fontFamily: "Inter, sans-serif", fontSize: "22px", fontWeight: "bold", marginLeft: "10px" }}>Recapify</p>
       </div>
