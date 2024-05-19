49d3053af9b050880f19eba067ae94f9044a80ea
Branden Phan <branden.phan@gmail.com>
2024-05-19 03:36:50 -0400

    Now finished!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Logo from "./RecapifyLogo.png";
 
 const App: React.FC = () => {
   return (
     <div style={{padding: "10px 20px", width: "90%" }}>
-      <p style={{ fontFamily: "Inter, sans-serif", fontSize: "22px", fontWeight: "bold" }}>Recapify</p>
-      <p style={{ fontFamily: "Inter, sans-serif", fontSize: "16px" }}>
+      <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+        <img src={Logo} alt="RecapifyLogo" height="25px" width="25px" style={{ marginBottom: "3px" }} />
+        <p style={{ fontFamily: "Inter, sans-serif", fontSize: "22px", fontWeight: "bold", marginLeft: "10px" }}>Recapify</p>
+      </div>
+      <p style={{ fontFamily: "Inter, sans-serif", fontSize: "16px", marginTop: "-2px" }}>
         Elevate your browsing experience with Recapify - an innovative AI-powered technology that summarizes any text on web pages with just a click. 
         Get the most out of every page.
       </p>
